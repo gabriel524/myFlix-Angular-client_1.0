04f14ebbf3552f817f43aafb59a4cd53dcbdb550
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-12-04 22:17:54 +0100

    file update for code inprovement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -15,6 +16,14 @@ export class AppComponent {
 openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
 // Assigning the dialog a width
+    width: '280px'
+    });
+  }
+
+ // This is the function that will open the dialog when the login button is clicked  
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+    // Assigning the dialog a width
     width: '280px'
     });
   }
