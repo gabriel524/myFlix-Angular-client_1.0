1eaebab90e0cd17e8bdb9b83500c62338cadbf41
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-12-06 02:32:42 +0100

    error return call update

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -156,6 +156,7 @@ export class FetchApiDataService {
         `Error Status code ${error.status},` + `Error body is: ${error.error}`
       );
     }
-    return throwError("Something bad happened; please try again later");
+    return throwError(() => new Error("Something bad happened; please try again later"));
   }
+  
 }
